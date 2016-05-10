8f7f3080ce1fad0d57606bc63b6ff548d13b81e6
James <jamesrp13@gmail.com>
2016-05-09 21:52:04 -0600

    Added DateHelper

--- Alarm/Alarm.swift
+++ Alarm/Alarm.swift
@@ -13,32 +13,9 @@ class Alarm: Equatable {
     var name: String
     var enabled: Bool
     
-    private var calendar: NSCalendar {
-        return NSCalendar.currentCalendar()
-    }
-    
-    private var thisMorningAtMidnight: NSDate? {
-        let components = calendar.components([.Month, .Day, .Year], fromDate: NSDate())
-        components.second = 0
-        components.minute = 0
-        components.hour = 0
-        components.nanosecond = 0
-        return calendar.dateFromComponents(components)
-    }
-    
-    private var tomorrowMorningAtMidnight: NSDate? {
-        let components = calendar.components([.Month, .Day, .Year], fromDate: NSDate())
-        components.second = 0
-        components.minute = 0
-        components.hour = 0
-        components.nanosecond = 0
-        guard let date = calendar.dateFromComponents(components) else {return nil}
-        return NSDate(timeInterval: 24*60*60, sinceDate: date)
-    }
-    
     var fireDate: NSDate? {
-        guard let thisMorningAtMidnight = thisMorningAtMidnight,
-            tomorrowMorningAtMidnight = tomorrowMorningAtMidnight where enabled else {return nil}
+        guard let thisMorningAtMidnight = DateHelper.thisMorningAtMidnight,
+            tomorrowMorningAtMidnight = DateHelper.tomorrowMorningAtMidnight where enabled else {return nil}
         let fireDateFromThisMorning = NSDate(timeInterval: fireTimeFromMidnight, sinceDate: thisMorningAtMidnight)
         let fireDateFromTomorrowMorning = NSDate(timeInterval: fireTimeFromMidnight, sinceDate: tomorrowMorningAtMidnight)
         if fireDateFromThisMorning.compare(NSDate()) == .OrderedAscending {
